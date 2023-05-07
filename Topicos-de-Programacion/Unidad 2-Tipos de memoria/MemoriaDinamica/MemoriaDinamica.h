1aed0866aa0592b6fa989c528dbf8d872d50c368
SolF <sacha.vrisky16@gmail.com>
2023-05-06 23:06:32 -0300

    Se implmentan ejemplos de memoria dinamica

---
+++ Topicos-de-Programacion/Unidad 2-Tipos de memoria/MemoriaDinamica/MemoriaDinamica.h
@@ -0,0 +1,12 @@
+#ifndef MEMORIADINAMICA_H_INCLUDED
+#define MEMORIADINAMICA_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+
+#endif // MEMORIADINAMICA_H_INCLUDED
